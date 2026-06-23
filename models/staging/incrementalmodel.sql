790af71c65f24c34e5ddd1e4d736091222c7a7af
Deepika Patlolla <dpatloll@cisco.com>
2026-06-23 17:30:07 +0000

    commit

---
+++ models/staging/incrementalmodel.sql
@@ -0,0 +1,18 @@
+{{
+    config(materialized='incremental', unique_key = 'order_id')
+}}
+select 
+order_id,
+customer_id,
+order_date,
+amount
+from {{source('raw','orders')}}
+
+{
+    % if is_incremental() %
+
+}
+where order_date > (select max(order_date) from {{this}}
+
+{ % endif % }
+}
