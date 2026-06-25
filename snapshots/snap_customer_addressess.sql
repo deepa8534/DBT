f7eaa8608af7c63244059dc5e9f7bb7eceb89378
Deepika Patlolla <dpatloll@cisco.com>
2026-06-25 07:14:10 +0000

    commit

--- snapshots/snap_customer_addressess.sql
+++ snapshots/snap_customer_addressess.sql
@@ -12,6 +12,6 @@ check_cols = ['street','city','state','pincode','is_default']
 
 }}
 select address_id, customer_id, address_type, street, city,state,pincode, is_default
-from  {{ source('customers','customer_address') }}
+from  {{source('customers','customer_address')}}
 
 {% endsnapshot %}
